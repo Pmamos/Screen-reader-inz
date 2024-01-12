f1a0b99d36ad14ecd8c3d585f150670639152674
Pmamos <80070974+Pmamos@users.noreply.github.com>
2024-01-13 00:29:42 +0100

    Add files via upload

--- application/application.m
+++ application/application.m
@@ -1,53 +1,59 @@
-
 function application(obj, v, size_p, figure_cutter, detector, detector_class, outputModel)
 global last_pos center_pos;
-persistent queue   ind_start  ind_end previousScreenPart pos last_obj;
-flaga_object = 0;
-if isempty(queue)
-    % Wywołaj funkcję Pythona do odczytu położenia myszy
-    pos = py.pyautogui.position();
+persistent queue ind_start ind_end previousScreenPart pos last_obj;
 
-    % Konwertuj wynik na tablicę NumPy
+if isempty(queue)
+    % Inicjalizacja kolejki i pozostałych zmiennych
+    pos = py.pyautogui.position();
     pos = double(py.array.array('d', pos));
-    % Initialize two independent queues
+    
+    % Inicjalizacja dwóch niezależnych kolejek
     queue1 = pos(1) * ones(1, 2);
     queue2 = pos(2) * ones(1, 2);
-
-    % Combine the queues into a single matrix
+    
+    % Połączenie kolejek w jedną macierz
     queue = [queue1; queue2];
     ind_start = 1;
     ind_end = 2;
     previousScreenPart = 'a';
     last_obj = '';
 end
+
 last_pos = pos;
-[pos, ind_start, ind_end, queue, movementDirection, currentScreenPart] = mouse_position(queue,ind_start, ind_end, flaga_object);
+
+% Pobranie nowej pozycji myszy i aktualizacja kolejki
+[pos, ind_start, ind_end, queue, movementDirection, currentScreenPart] = mouse_position(queue, ind_start, ind_end);
+
+% Przygotowanie obrazu i detekcja obiektów
 img = cutter(v, size_p, pos);
-
 [bboxes, ~, labels, ~] = detect_object(img, detector, detector_class);
+
+% Odtworzenie dźwięku, jeśli wykryto obiekt
 if(~isempty(bboxes))
-    make_sound()
+    make_sound();
 end
-[min_ind, flaga_zblizanie, komunikat_end, center_pos] = inObject(bboxes,size_p,pos, movementDirection);
 
+% Sprawdzenie, czy jesteśmy wewnątrz obiektu
+[min_ind, flaga_zblizanie, komunikat_end, center_pos] = inObject(bboxes, size_p, pos, movementDirection);
 
-% ocrResults = ocr(img,Language=outputModel);
+% Rozpoznawanie tekstu na obrazie
 if min_ind > 0
-    [~,text] = whichText(min_ind,img, bboxes, outputModel);
+    [~, text] = whichText(min_ind, img, bboxes, outputModel);
     text = strjoin(text, ' ');
     obiekt = string(labels(min_ind));
 else
-    [~,text] = readText(img, outputModel);
+    [~, text] = readText(img, outputModel);
     text = strjoin(text, ' ');
     obiekt = " ";
 end
-% imgName = insertObjectAnnotation(detectedImg,"rectangle",ocrResults.WordBoundingBoxes,ocrResults.Words);
-figure(figure_cutter)
 
+% Wyświetlenie obrazu z oznaczonymi obiektami
+figure(figure_cutter);
+set(figure_cutter, 'Position', [1 1 100 100]);
 
-
-previousScreenPart = read(obj, currentScreenPart, previousScreenPart,obiekt, flaga_zblizanie, komunikat_end, text,last_obj);
-last_obj = obiekt +' '+ text;
+% Wywołanie funkcji do obsługi odczytanego tekstu i aktualizacja zmiennych
+[previousScreenPart, komunikat_obiekt] = read(obj, currentScreenPart, previousScreenPart, obiekt, flaga_zblizanie, komunikat_end, text, last_obj);
+last_obj = komunikat_obiekt;
 end
 
 % Funkcja obsługująca zatrzymywanie timera po kliknięciu przycisku
@@ -56,4 +62,3 @@ stop(timerObj);
 delete(timerObj);
 disp('Timer zatrzymany.');
 end
-
